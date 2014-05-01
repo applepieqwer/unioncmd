15ae1a9c21584849c73b05662edb0123a627ac74
applepieqwer <applepieqwer@gmail.com>
2014-05-01 15:47:55 +0800

    Update uc_init.sh

--- uc_init.sh
+++ uc_init.sh
@@ -8,3 +8,4 @@ git clone https://github.com/applepieqwer/unioncmd.git
 chmod -R a+rwx /home/applepie/unioncmd 
 cp /home/applepie/unioncmd/uc_init.sh /bin/uc_init.sh
 chmod +x /bin/uc_init.sh
+sh /home/applepie/uc_firstrun.py
