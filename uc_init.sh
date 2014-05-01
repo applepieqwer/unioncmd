8d6f63af0bf52b358ecaf548e0d4ad0eb32a1fda
applepieqwer <applepieqwer@gmail.com>
2014-05-01 14:48:38 +0800

    Create uc_init.sh

---
+++ uc_init.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#must place this file at /home/applepie
+
+rm -rf /home/applepie/unioncmd
+cd /home/applepie
+git clone https://github.com/applepieqwer/unioncmd.git
+cp /home/applepie/unioncmd/uc_init.sh /home/applepie/uc_init.sh
