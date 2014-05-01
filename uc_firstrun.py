72c6051d08776c5761281783197699ec99db0dcd
applepieqwer <applepieqwer@gmail.com>
2014-05-01 15:59:50 +0800

    Update uc_firstrun.py

--- uc_firstrun.py
+++ uc_firstrun.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
+from os import system
+
+def setup_cron_file():
+  system('cp /home/applepie/unioncmd/uc /etc/cron.d/uc')
+  system('service cron restart')
+  
+if __name__ == '__main__':
+  setup_cron_file()
 
