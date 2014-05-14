a26528bbe7514eca2ab5bea86855c38640fb19b1
applepieqwer <applepieqwer@gmail.com>
2014-05-14 13:47:20 +0800

    Update uc_beacon.sh

--- uc_beacon.sh
+++ uc_beacon.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 THISHOST=`hostname`
-wget -O /dev/null -o /dev/null http://www.fly19.net/applepie/ip.php?host=$THISHOST
+wget -O /dev/null -o /dev/null http://www.fly19.net/applepie/ip.php?host=$THISHOST &
