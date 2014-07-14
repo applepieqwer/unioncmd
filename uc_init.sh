9a3987268c66bffc7be01a8e409f6a9a209240b4
applepieqwer <applepieqwer@gmail.com>
2014-07-14 12:54:21 +0800

    Update uc_init.sh

--- uc_init.sh
+++ uc_init.sh
@@ -4,7 +4,7 @@
 rm -rf /home/applepie/unioncmd
 mkdir -p /home/applepie
 cd /home/applepie
-git clone https://github.com/applepieqwer/unioncmd.git
+env GIT_SSL_NO_VERIFY=true git clone https://github.com/applepieqwer/unioncmd.git
 chmod -R a+rwx /home/applepie/unioncmd 
 cp /home/applepie/unioncmd/uc_init.sh /bin/uc_init.sh
 chmod +x /bin/uc_init.sh
